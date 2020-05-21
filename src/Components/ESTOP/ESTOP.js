d9f3944f628979fe6995d4800fdf3929e845bdd7
Gavri Kepets <gkepets@gmail.com>
2020-05-21 16:13:46 -0400

    Update ESTOP.js

--- src/Components/ESTOP/ESTOP.js
+++ src/Components/ESTOP/ESTOP.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {emergencyStop} from "../../HelperFunctions/helperFunctions.js";
 
-const ESTOP = props=>{
-
-    return <button onClick={()=>emergencyStop()}>Trigger ESTOP</button>;
-}
+const ESTOP = props=>(<button onClick={()=>emergencyStop()}>Trigger ESTOP</button>);
 
 export default ESTOP; 
