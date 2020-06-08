036ecd4841726a6300df7382e4f6d2b6a6118b72
root <root@LAPTOP-Q4MFCHAT.localdomain>
2020-06-08 16:33:47 -0400

    testStates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ESTOP from './Components/ESTOP'; 
-import Centerpiece from './Components/Centerpiece'; 
+import ESTOP from './Components/ESTOP';
+import Centerpiece from './Components/Centerpiece';
+import AutoDisplay from './Components/AutoDisplay';
 
-const App = () => (<div><Centerpiece></Centerpiece></div>);
+const App = () => (<div><AutoDisplay></AutoDisplay></div>);
 
 export default App;
